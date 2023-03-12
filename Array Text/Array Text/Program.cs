2f8936fb58a28a83c63481c39ac02b0caf069a7a
unknown <elvin@example.com>
2023-03-12 23:54:49 +0400

    .

---
+++ Array Text/Array Text/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+//Task1
+//int[] arr = { 2, 4, 5, 6, 8, 2, 3,200 };
+//int sum = 0;
+//int cem = 0;
+//for (int i = 0; i < arr.Length; i++)
+//{
+//    sum += arr[i];
+
+
+//}
+//Console.WriteLine(sum);
+
+//Task2
+//string[] arr = { "salamr", "necesen", "iki", "dord,","uc" };
+
+
+//for (int i = 0; i < arr.Length; i++)
+//{
+//   if(arr[i].Length < 4)
+//    {
+//        Console.WriteLine(arr[i]);
+//    }
+
+//}  
